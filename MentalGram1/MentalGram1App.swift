4052d95fc81bb4b87cd1c5865121f4e9e5f506dc
nelson <nga250530@gmail.com>
2026-02-08 09:48:25 +0000

    Initial Commit

---
+++ MentalGram1/MentalGram1App.swift
@@ -0,0 +1,17 @@
+//
+//  MentalGram1App.swift
+//  MentalGram1
+//
+//  Created by NELSON SUÁREZ ARTEAGA on 8/2/26.
+//
+
+import SwiftUI
+
+@main
+struct MentalGram1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
